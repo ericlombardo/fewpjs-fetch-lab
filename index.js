55a3876ac06730776f260137d2ebb333cf39fd5d
Eric <eric.m.lombardo@gmail.com>
2021-05-24 17:46:05 -0400

    commit tree b165fdf389c21b0925f5690dfe2c789453ab0130

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books') // sends request for data
+    .then(resp => resp.json()) // sets variable name to resp and turns into json object
+    .then(json => renderBooks(json)); // sets var. name to json and passes to function 
+    
+  }
   
-}
-
-function renderBooks(books) {
+  function renderBooks(books) {
+  debugger;
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
